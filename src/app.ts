d2ee4f7747deca8a1fb0aa9b1c6d7794f9a03d6c
HappyBoi04092004 <163809990+HappyBoi04092004@users.noreply.github.com>
2025-11-16 16:17:20 +0700

    setup tham số môi trường

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
+import { Express } from 'express';// su dung kieu Express
+import 'dotenv/config';//import dotenv tu dong load file .env
+//require('dotenv').config();
+
 const express = require("express");
 const app = express();
 
-const PORT =3000;
+const PORT =process.env.PORT || 3000;// Dieu kien hoac neu ko chay thi chay tren 3000
 app.get('/', (req,res) => {
     res.send ('App is running and update');
     }
@@ -14,5 +18,6 @@ app.get('/1', (req,res) => {
 app.listen(PORT, () => {
     console.log(`App is running on port : ${PORT} `);
     console.log(`Link vao thang web: http://localhost:${PORT}`);
+    console.log('ENV PORT:', process.env.PORT);
 })
 
