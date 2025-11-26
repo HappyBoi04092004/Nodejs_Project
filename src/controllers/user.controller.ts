056d680a023ebcdd51b18443df55e88090230eba
HappyBoi04092004 <163809990+HappyBoi04092004@users.noreply.github.com>
2025-11-26 10:12:32 +0700

    Cách để lưu được data

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { handleCreateUser } from "../services/user-service";
 
 const getHomePage = (req:Request, res:Response) => {
     return res.render("home.ejs")
@@ -7,7 +8,9 @@ const getCreateUserPage = (req:Request, res:Response) => {
     return res.render("create-user.ejs")
 } 
 const postCreateUserPage = (req:Request, res:Response) => {
-    console.log(">>>check req body:", req.body);
+    console.log(">>>check req body:", req.body);    
+    //handle create user
+    handleCreateUser(req.body.name, req.body.email);
     return res.redirect("/")
 } 
 export { getHomePage ,getCreateUserPage,postCreateUserPage};//export ham getHomePage de su dung o file khac va co the su fung nhieu file
---
+++ src/services/user-service.ts
@@ -0,0 +1,11 @@
+const handleCreateUser = (
+    name:string,
+    email:string) => {
+
+        //insert user vao database
+
+        //return kq
+        console.log(">>> Created user:", name, email);
+
+}
+export { handleCreateUser };
