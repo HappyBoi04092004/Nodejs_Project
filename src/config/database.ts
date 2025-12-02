8390b39f2e6767c25aa6bec3d8c7f5c44af5d7e5
HappyBoi04092004 <163809990+HappyBoi04092004@users.noreply.github.com>
2025-12-02 14:40:07 +0700

    Cách liên kết vs database đã tạo với fakedata

---
+++ src/config/database.ts
@@ -0,0 +1,29 @@
+// Get the client
+import mysql from 'mysql2/promise';
+
+
+
+const getConnection = async() => {
+    // Create the connection to database
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: '123456',
+        database: 'tuhocnodejs',
+});
+
+// A simple SELECT query
+try {
+  const [results, fields] = await connection.query(
+    'SELECT * FROM `users` '
+  );
+
+  console.log(results); // results contains rows returned by server
+  console.log(fields); // fields contains extra meta data about results, if available
+} catch (err) {
+  console.log(err);
+}
+
+}
+export default  getConnection ;
