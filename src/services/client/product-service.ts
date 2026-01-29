d262bf85927804f50fa732b6000a74e5707ae8fc
HappyBoi04092004 <163809990+HappyBoi04092004@users.noreply.github.com>
2026-01-29 09:41:26 +0700

    Update cart to detail

--- src/services/client/product-service.ts
+++ src/services/client/product-service.ts
@@ -72,6 +72,44 @@ const addProductToCart = async (quantity: number, productId: number, user: any) 
 
     if(cart){
         //update cart
+        //cap nhat sum gio hang 
+        await prisma.cart.update({
+            where:{id:cart.id},
+            data:{
+                sum: {
+                    increment: quantity,
+                }
+            }
+        })
+
+        //cap nhat cart detail
+        //neu chua co cap nhat quantity
+        //upsert= update + insert
+        const currentCartDetail = await prisma. cartDetail.findFirst({
+            where:{
+                productId: productId,
+                cartId:cart.id
+            }
+        })
+
+
+        await prisma.cartDetail.upsert({
+            where: {
+                id:currentCartDetail?. id ?? 0
+            },
+            update: {
+                quantity:{
+                    increment:quantity
+                }
+            },
+            create: {
+                price: product.price,
+                quantity: quantity,
+                productId:productId,
+                cartId: cart.id 
+        },
+            })
+
     }else{
         //create cart
         await prisma.cart.create({
